09e2b5f2e80bbb0aede98161b4e8a45e30999c41
Norman Khan <norman1000@gmail.com>
2018-12-05 10:10:58 +0000

    Did things

--- FluxQuery.py
+++ FluxQuery.py
@@ -68,7 +68,7 @@ def mjd2year(times):    #Converts mjd time to decimal years
 
 h = Heasarc()
 
-object_name = 'NGC6946'
+object_name = 'ngc5402'
 
 mission1 = ['xmmssc', 'XMM']        #XMM
 mission2 = ['swiftmastr', 'Swift']  #Swift
@@ -93,34 +93,46 @@ try:
     table8 = h.query_object(object_name, mission=mission8[0], fields='All')
 except TypeError:
     pass
+
 ######################XMM FLUXES#######################
-    
 softPN = np.asarray(table1['PN_1_FLUX'] + table1['PN_2_FLUX'])
 hardPN = np.asarray(table1['PN_3_FLUX'] + table1['PN_4_FLUX'] + table1['PN_5_FLUX'])
 
+######################ROSAT COUNT RATE#################
+ROS_CR = np.asarray(table5['COUNT_RATE'])
+
 try:
     plt.scatter(mjd2year(np.array(table1['TIME'])), softPN, 
-                marker='x', label=mission1[1])
+                marker='x', label=mission1[1]+' soft')
+    plt.scatter(mjd2year(np.array(table1['TIME'])), hardPN, 
+                marker='x', label=mission1[1]+' hard')
+
+    plt.scatter(mjd2year(np.array(table2['START_TIME'])), 
+                np.zeros(len(np.array(table2['START_TIME']))), marker='x', label=mission2[1])
     
-    plt.scatter(mjd2year(np.array(table2['TIME'])), 
-                np.zeros(len(np.array(table2['TIME']))), marker='x', label=mission2[1])
     plt.scatter(mjd2year(np.array(table3['TIME'])), 
                 np.zeros(len(np.array(table3['TIME']))), marker='x', label=mission3[1])
+    
     plt.scatter(mjd2year(np.array(table4['TIME'])), 
                 np.zeros(len(np.array(table4['TIME']))), marker='x', label=mission4[1])
-    plt.scatter(mjd2year(np.array(table5['TIME'])), 
+    
+    plt.scatter(mjd2year(np.array(table5['TIME'])),
                 np.zeros(len(np.array(table5['TIME']))), marker='x', label=mission5[1])
+    
     plt.scatter(mjd2year(np.array(table6['TIME'])), 
                 np.zeros(len(np.array(table6['TIME']))), marker='x', label=mission6[1])
+    
     plt.scatter(mjd2year(np.array(table7['START_TIME'])), 
-                np.zeros(len(np.array(table7['START_TIME']))), marker='x', label=mission7[1])      
+                np.zeros(len(np.array(table7['START_TIME']))), marker='x', label=mission7[1]) 
+     
     plt.scatter(mjd2year(np.array(table8['TIME'])), 
                 np.zeros(len(np.array(table8['TIME']))), marker='x', label=mission8[1])  
 except NameError:
     pass
 
-plt.ylim(0,1.1*np.nanmax(softPN))
+plt.ylim(0,1.1*np.nanmax(np.concatenate((softPN, hardPN))))
+
 plt.title(object_name)
 plt.xlabel('TIME')
-plt.ylabel('Flux $$')
+plt.ylabel('Flux')
 plt.legend()
