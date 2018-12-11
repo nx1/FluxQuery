1bf3b4e1797f1e93046c9928dd8697fc694730ca
Norman Khan <norman1000@gmail.com>
2018-12-11 14:47:34 +0000

    MADE IT AUTOMATIC

--- FluxQuery.py
+++ FluxQuery.py
@@ -51,73 +51,58 @@ h = Heasarc()
 
 object_name = 'ngc1313'
 
-mission1 = ['xmmssc', 'XMM']        #XMM
-mission2 = ['swiftmastr', 'Swift']  #Swift
-mission3 = ['NUMASTER', 'NuSTAR']   #NuSTAR
-mission4 = ['CHANMASTER', 'Chandra']#Chandra
-mission5 = ['RASS2RXS', 'ROSAT']    #ROSAT
-mission6 = ['fermilasp', 'Fermi']   #Fermi
-mission7 = ['mpcraw', 'Einstein']   #Einstien
-mission8 = ['suzaxislog', 'SUZAKU'] #SUZAKU
-mission9 = ['nicermastr', 'NICER'] #SUZAKU
+'''
+Missions with time given as ['TIME']:
+    xmmssc, NUMASTER, CHANMASTER, RASS2RXS, fermilasp, suzaxislog, nicermastr
+Missions with time given as ['START_TIME']:
+    swiftmastr, mpcraw
+'''
 
+M = {
+     'XMM': 'xmmssc', 
+     'Swift': 'swiftmastr', 
+     'NuSTAR': 'NUMASTER', 
+     'Chandra': 'CHANMASTER', 
+     'ROSAT': 'RASS2RXS', 
+     'Fermi': 'fermilasp', 
+     'Einstein': 'mpcraw', 
+     'SUZAKU': 'suzaxislog', 
+     'NICER': 'nicermastr', 
+     }  #Dictionary for storing missions
 
+T = {}  #Dictonary for storing table from queries
 
+for i in M:
+    try:
+        T[i] = h.query_object(object_name, mission=M[i], fields='All')
+    except TypeError:
+        print('TypeError:', i, '| probably due to no observation of source')
+        pass
+    
+for i in T:
+    try:
+        if i == 'XMM':
+            softPN = np.asarray(T[i]['PN_1_FLUX'] + T[i]['PN_2_FLUX'])
+            hardPN = np.asarray(T[i]['PN_3_FLUX'] + T[i]['PN_4_FLUX'] + T[i]['PN_5_FLUX'])
+            
+            plt.scatter(mjd2year(np.array(T[i]['TIME'])), softPN,
+                            marker='x', label= i + ' soft')
+            plt.scatter(mjd2year(np.array(T[i]['TIME'])), hardPN,
+                            marker='x', label=i +' hard')
 
-try: 
-    table1 = h.query_object(object_name, mission=mission1[0], fields='All')
-    table2 = h.query_object(object_name, mission=mission2[0], fields='All')
-    table3 = h.query_object(object_name, mission=mission3[0], fields='All')
-    table4 = h.query_object(object_name, mission=mission4[0], fields='All')
-    table5 = h.query_object(object_name, mission=mission5[0], fields='All')
-    table6 = h.query_object(object_name, mission=mission6[0], fields='All')
-    table7 = h.query_object(object_name, mission=mission7[0], fields='All')
-    table8 = h.query_object(object_name, mission=mission8[0], fields='All')
-    table9 = h.query_object(object_name, mission=mission9[0], fields='All')
-except TypeError:
-    pass
-
-######################XMM FLUXES#######################
-softPN = np.asarray(table1['PN_1_FLUX'] + table1['PN_2_FLUX'])
-hardPN = np.asarray(table1['PN_3_FLUX'] + table1['PN_4_FLUX'] + table1['PN_5_FLUX'])
-
-######################ROSAT COUNT RATE#################
-ROS_CR = np.asarray(table5['COUNT_RATE'])
-
-try:
-    plt.scatter(mjd2year(np.array(table1['TIME'])), softPN, 
-                marker='x', label=mission1[1]+' soft')
+        else:
+            plt.scatter(mjd2year(np.array(T[i]['TIME'])),
+                        np.zeros(len(np.array(T[i]['TIME']))), marker='x', label=i)
+        
+    except NameError:
+        print('NameError:', i)
+        pass
+    
+    except KeyError:
+        print('KeyError, trying to use table header START_TIME')
+        plt.scatter(mjd2year(np.array(T[i]['START_TIME'])),
+                    np.zeros(len(np.array(T[i]['START_TIME']))), marker='x', label=i)
  
-    
-    plt.scatter(mjd2year(np.array(table1['TIME'])), hardPN, 
-                marker='x', label=mission1[1]+' hard')
-    
-    plt.scatter(mjd2year(np.array(table2['START_TIME'])), 
-                np.zeros(len(np.array(table2['START_TIME']))), marker='x', label=mission2[1])
-    
-    plt.scatter(mjd2year(np.array(table3['TIME'])), 
-                np.zeros(len(np.array(table3['TIME']))), marker='x', label=mission3[1])
-    
-    plt.scatter(mjd2year(np.array(table4['TIME'])), 
-                np.zeros(len(np.array(table4['TIME']))), marker='x', label=mission4[1])
-    
-    plt.scatter(mjd2year(np.array(table5['TIME'])),
-                np.zeros(len(np.array(table5['TIME']))), marker='x', label=mission5[1])
-    
-    plt.scatter(mjd2year(np.array(table6['TIME'])), 
-                np.zeros(len(np.array(table6['TIME']))), marker='x', label=mission6[1])
-    
-    plt.scatter(mjd2year(np.array(table7['START_TIME'])), 
-                np.zeros(len(np.array(table7['START_TIME']))), marker='x', label=mission7[1]) 
-     
-    plt.scatter(mjd2year(np.array(table8['TIME'])), 
-                np.zeros(len(np.array(table8['TIME']))), marker='x', label=mission8[1])  
-    
-    plt.scatter(mjd2year(np.array(table9['TIME'])), 
-                np.zeros(len(np.array(table9['TIME']))), marker='x', label=mission9[1])  
-except NameError:
-    pass   
-
 
 plt.ylim(0,1.1*np.nanmax(np.concatenate((softPN, hardPN))))
 
